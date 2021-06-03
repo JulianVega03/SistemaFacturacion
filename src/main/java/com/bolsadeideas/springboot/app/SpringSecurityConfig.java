0a95a47af07853ae5611880ad3fc17edbec4bc39
JulianVega03 <33796971+JulianVega03@users.noreply.github.com>
2021-06-03 15:42:47 -0500

    upload files

---
+++ src/main/java/com/bolsadeideas/springboot/app/SpringSecurityConfig.java
@@ -0,0 +1,69 @@
+package com.bolsadeideas.springboot.app;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.bolsadeideas.springboot.app.auth.filter.JWTAuthenticationFilter;
+import com.bolsadeideas.springboot.app.auth.filter.JWTAuthorizationFilter;
+import com.bolsadeideas.springboot.app.auth.service.JWTService;
+/*import com.bolsadeideas.springboot.app.auth.handler.LoginSuccessHandler;*/
+import com.bolsadeideas.springboot.app.models.service.JpaUserDetailsService;
+
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter{
+
+	/*
+	 * @Autowired
+	private LoginSuccessHandler successHandler;
+	*/
+	
+	@Autowired
+	private JpaUserDetailsService userDetailsService;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private JWTService jwtService;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+		http.authorizeRequests().antMatchers("/", "/css/**", "/js/**", "/images/**", "/listar**", "/locale").permitAll()
+		.anyRequest().authenticated()
+		
+		/*
+		 * .and()
+		    .formLogin()
+		        .successHandler(successHandler)
+		        .loginPage("/login")
+		    .permitAll()
+		.and()
+		.logout().permitAll()
+		.and()
+		.exceptionHandling().accessDeniedPage("/error_403")
+		*
+		*/
+		
+		.and()
+		.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtService))
+		.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtService))
+		.csrf().disable()
+		.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+	}
+
+	@Autowired
+	public void configurerGlobal(AuthenticationManagerBuilder build) throws Exception
+	{
+		build.userDetailsService(userDetailsService)
+		.passwordEncoder(passwordEncoder);
+	}
+}
